9ea97d6710bb24626de3945e066dcc5f07035c26
topdeveloper <topdeveloper0908@gmail.com>
2022-10-07 05:30:25 +0300

    update

--- lib/util/ses.ts
+++ lib/util/ses.ts
@@ -1,4 +1,5 @@
 import { emailTemplates, getEmailTemplate } from './emailTemplates';
+const sgMail = require('@sendgrid/mail');
 
 export const sendEmail = async ({
     to,
@@ -14,7 +15,7 @@ export const sendEmail = async ({
     try {
         // const data = await sesClient.send(new SendEmailCommand(params));
         // return data.$metadata; // For unit tests.
-        const sgMail = require('@sendgrid/mail');
+      
         sgMail.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_API_KEY);
         const msg = {
             to: to,
@@ -23,9 +24,16 @@ export const sendEmail = async ({
             text: emailTemplate?.body,
             // html: '<strong>and easy to do anywhere, even with Node.js</strong>',
         };
+        const data = {
+            to: "kattymokh@gmail.com",
+            from: "topdeveloper@gmail.com",
+            subject: `New message from`,
+            text: 'message',
+            html: 'message'.replace(/\r\n/g, "<br />"),
+          };
         //ES6
         try {
-            await sgMail.send(msg);
+            await sgMail.send(data);
             return 200;
         } catch (error: any) {
             console.error(error);
--- pages/login/pwd-recovery.tsx
+++ pages/login/pwd-recovery.tsx
@@ -1,12 +1,13 @@
 import { ReactElement } from 'react';
 import { useForm } from 'react-hook-form';
 import { useRecoilState } from 'recoil';
+import { toast } from 'react-toastify';
 
 import { Container, FormControl, FormLabel, InputGroup, InputRightElement, Flex, Input, Button, Box, Heading, Link } from '@chakra-ui/react'
 
 import apiClient from '~/client';
 import { isLoadingState } from '~/store';
-import { toast } from 'react-toastify';
+import Fallback from '~/components/features/Fallback';
 
 const PwdRecoveryPage = () => {
     const [isLoading, setIsLoading] = useRecoilState(isLoadingState);
@@ -19,7 +20,6 @@ const PwdRecoveryPage = () => {
 
     const onSubmit = async ({ email }: { email: string }) => {
         setIsLoading(true);
-
         await apiClient
             .get(`/api/auth/reset-password?email=${email}`)
             .then((res) => {
@@ -43,45 +43,48 @@ const PwdRecoveryPage = () => {
     };
 
     return (
-        <Container>
-            <Box mt={'20'} p='10' borderWidth='1px' borderRadius='lg' overflow='hidden' textAlign={'center'}>
-                <form
-                    className="flex justify-center flex-col w-full"
-                    onSubmit={handleSubmit(onSubmit)}
-                >
-                    <Heading mb={'5'} textAlign={'center'}>Reset Password</Heading>
-                    <Heading size={'1'} mb={'8'} color="gray.600" textAlign={'center'}>
-                        Enter your email address and we&apos;ll send you reset password
-                        instructions.
-                    </Heading>
-                    <Input  type="email"
-                        placeholder="Enter your Email"
-                        {...register('email', { required: true })}
-                    required/>
-                    <Button
-                        py={4}
-                        my={4}
-                        fontWeight={'bold'}
-                        size={"md"}
-                        colorScheme='teal'
-                        type='submit'
+        <>
+            <Container>
+                <Box mt={'20'} p='10' borderWidth='1px' borderRadius='lg' overflow='hidden' textAlign={'center'}>
+                    <form
+                        className="flex justify-center flex-col w-full"
+                        onSubmit={handleSubmit(onSubmit)}
                     >
-                        Submit
-                    </Button>
-                    <Flex gap={3} color={'gray.600'} mb={4} alignItems={'center'} fontSize="sm">
-                        <Box height={'1px'} bg={'gray.300'} flex={1}></Box> 
-                        <Heading size={"md"}>Remember your password?</Heading>
-                        <Box height={'1px'} bg={'gray.300'} flex={1}></Box> 
-                    </Flex>
-                    <Link
-                        href="/login"
-                        className="rounded-full py-4 border-black border-2 text-sm font-bold mb-8 text-center"
-                    >
-                        Log In
-                    </Link>
-                </form>
-            </Box>
-        </Container>
+                        <Heading mb={'5'} textAlign={'center'}>Reset Password</Heading>
+                        <Heading size={'1'} mb={'8'} color="gray.600" textAlign={'center'}>
+                            Enter your email address and we&apos;ll send you reset password
+                            instructions.
+                        </Heading>
+                        <Input  type="email"
+                            placeholder="Enter your Email"
+                            {...register('email', { required: true })}
+                        required/>
+                        <Button
+                            py={4}
+                            my={4}
+                            fontWeight={'bold'}
+                            size={"md"}
+                            colorScheme='teal'
+                            type='submit'
+                        >
+                            Submit
+                        </Button>
+                        <Flex gap={3} color={'gray.600'} mb={4} alignItems={'center'} fontSize="sm">
+                            <Box height={'1px'} bg={'gray.300'} flex={1}></Box> 
+                            <Heading size={"md"}>Remember your password?</Heading>
+                            <Box height={'1px'} bg={'gray.300'} flex={1}></Box> 
+                        </Flex>
+                        <Link
+                            href="/login"
+                            className="rounded-full py-4 border-black border-2 text-sm font-bold mb-8 text-center"
+                        >
+                            Log In
+                        </Link>
+                    </form>
+                </Box>
+            </Container>
+            {isLoading && <Fallback />}
+        </>
     );
 };
 
